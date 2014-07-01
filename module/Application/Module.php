267e1e29a96c87af3e6504cbb05fe118c28cc160
Gavin Rogers <mark@suttonsilver.co.uk>
2014-07-01 15:06:19 +0100

    doctrine entity config added

--- module/Application/Module.php
+++ module/Application/Module.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Zend Framework (http://framework.zend.com/)
  *
@@ -12,28 +13,26 @@ namespace Application;
 use Zend\Mvc\ModuleRouteListener;
 use Zend\Mvc\MvcEvent;
 
-class Module
-{
-    public function onBootstrap(MvcEvent $e)
-    {
-        $eventManager        = $e->getApplication()->getEventManager();
-        $moduleRouteListener = new ModuleRouteListener();
-        $moduleRouteListener->attach($eventManager);
-    }
+class Module {
 
-    public function getConfig()
-    {
-        return include __DIR__ . '/config/module.config.php';
-    }
+	public function onBootstrap(MvcEvent $e) {
+		$eventManager = $e->getApplication()->getEventManager();
+		$moduleRouteListener = new ModuleRouteListener();
+		$moduleRouteListener->attach($eventManager);
+	}
 
-    public function getAutoloaderConfig()
-    {
-        return array(
-            'Zend\Loader\StandardAutoloader' => array(
-                'namespaces' => array(
-                    __NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
-                ),
-            ),
-        );
-    }
+	public function getConfig() {
+		return include __DIR__ . '/config/module.config.php';
+	}
+
+	public function getAutoloaderConfig() {
+		return array(
+			'Zend\Loader\StandardAutoloader' => array(
+				'namespaces' => array(
+					__NAMESPACE__ => __DIR__ . '/src/' . __NAMESPACE__,
+				),
+			),
+		);
+	}
+
 }
---
+++ module/Users/src/Users/Entity/User.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Users\Entity;
+
+class User {
+
+	protected $id;
+	protected $username;
+	protected $password;
+
+	public function setId($id) {
+		$this->id = $id;
+		return $this;
+	}
+
+	public function getId() {
+		return $this->id;
+	}
+
+	public function setUsername($username) {
+		$this->username = $username;
+		return $this;
+	}
+
+	public function getUsername() {
+		return $this->username;
+	}
+
+	public function setPassword($password) {
+		$this->password = $password;
+		return $this;
+	}
+
+	public function getPassword() {
+		return $this->password;
+	}
+
+}
