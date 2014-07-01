85c09a70790e30132d11380d6c52b887ab778cb3
Gavin Rogers <gavinrog@gmail.com>
2014-07-01 20:48:38 +0100

    event manager added to mapper

--- module/Common/src/Common/Mapper/AbstractMapper.php
+++ module/Common/src/Common/Mapper/AbstractMapper.php
@@ -2,13 +2,17 @@
 
 namespace Common\Mapper;
 
-use Doctrine\ORM\EntityManager,
+use Zend\EventManager\EventManagerAwareInterface,
+    Zend\EventManager\EventManagerInterface,
+    Zend\EventManager\EventManager,
+    Doctrine\ORM\EntityManager,
     Common\Mapper\Exception\NoEntityException;
 
-abstract class AbstractMapper {
+abstract class AbstractMapper implements EventManagerAwareInterface {
 
     protected $entityClass;
     protected $entityManager;
+    protected $eventManager;
 
     public function __construct(EntityManager $entityManager) {
 
@@ -35,6 +39,17 @@ abstract class AbstractMapper {
     public function setEntityClass($entityClass) {
         $this->entityClass = $entityClass;
         return $this;
+    }
+
+    public function setEventManager(EventManagerInterface $eventManager) {
+        $this->eventManager = $eventManager;
+    }
+
+    public function getEventManager() {
+        if (!$this->eventManager) {
+            $this->setEventManager(new EventManager(array(__CLASS__, get_called_class())));
+        }
+        return $this->eventManager;
     }
 
     protected function getRepository() {
