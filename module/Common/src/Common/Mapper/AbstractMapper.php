efaf9b4c0928a2582772d3d9945eb65d2526d3ba
Gavin Rogers <gavinrog@gmail.com>
2014-07-01 20:16:35 +0100

    no need for get

--- module/Common/src/Common/Mapper/AbstractMapper.php
+++ module/Common/src/Common/Mapper/AbstractMapper.php
@@ -3,54 +3,54 @@
 namespace Common\Mapper;
 
 use Doctrine\ORM\EntityManager,
-	Common\Mapper\Exception\NoEntityException;
+    Common\Mapper\Exception\NoEntityException;
 
 abstract class AbstractMapper {
 
-	protected $entityClass;
-	protected $entityManager;
+    protected $entityClass;
+    protected $entityManager;
 
-	public function __construct(EntityManager $entityManager) {
+    public function __construct(EntityManager $entityManager) {
 
-		if (!$this->entityClass) {
-			throw new NoEntityException('You must set an entity class');
-		}
+        if (!$this->entityClass) {
+            throw new NoEntityException('You must set an entity class');
+        }
 
-		$this->entityManager = $entityManager;
-	}
+        $this->entityManager = $entityManager;
+    }
 
-	public function setEntityManager(EntityManager $entityManager) {
-		$this->entityManager = $entityManager;
-		return $this;
-	}
+    public function setEntityManager(EntityManager $entityManager) {
+        $this->entityManager = $entityManager;
+        return $this;
+    }
 
-	public function getEntityManager() {
-		return $this->entityManager;
-	}
+    public function getEntityManager() {
+        return $this->entityManager;
+    }
 
-	public function getEntityClass() {
-		return $this->entityClass;
-	}
+    public function getEntityClass() {
+        return $this->entityClass;
+    }
 
-	public function setEntityClass($entityClass) {
-		$this->entityClass = $entityClass;
-		return $this;
-	}
+    public function setEntityClass($entityClass) {
+        $this->entityClass = $entityClass;
+        return $this;
+    }
 
-	protected function getRepository() {
-		return $this->getEntityManager()->getRepository($this->getEntityClass());
-	}
+    protected function getRepository() {
+        return $this->getEntityManager()->getRepository($this->entityClass);
+    }
 
-	public function fetchOne($id) {
-		return $this->getRepository()->find($id);
-	}
+    public function fetchOne($id) {
+        return $this->getRepository()->find($id);
+    }
 
-	public function fetchAll() {
-		return $this->getRepository()->fetchAll();
-	}
+    public function fetchAll() {
+        return $this->getRepository()->fetchAll();
+    }
 
-	public function fetchBy(array $criteria) {
-		return $this->getRepository()->findBy($criteria);
-	}
+    public function fetchBy(array $criteria) {
+        return $this->getRepository()->findBy($criteria);
+    }
 
 }
