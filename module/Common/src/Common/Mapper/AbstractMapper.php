402b1119799e0c96bf384ee4dd76b291efee6cb9
Gavin Rogers <mark@suttonsilver.co.uk>
2014-07-02 15:35:43 +0100

    removed spaces

--- module/Common/src/Common/Mapper/AbstractMapper.php
+++ module/Common/src/Common/Mapper/AbstractMapper.php
@@ -3,69 +3,67 @@
 namespace Common\Mapper;
 
 use Zend\EventManager\EventManagerAwareInterface,
-    Zend\EventManager\EventManagerInterface,
-    Zend\EventManager\EventManager,
-    Doctrine\ORM\EntityManager,
-    Common\Mapper\Exception\NoEntityException;
+	Zend\EventManager\EventManagerInterface,
+	Zend\EventManager\EventManager,
+	Doctrine\ORM\EntityManager,
+	Common\Mapper\Exception\NoEntityException;
 
 abstract class AbstractMapper implements EventManagerAwareInterface {
 
-    protected $entityClass;
-    protected $entityManager;
-    protected $eventManager;
+	protected $entityClass;
+	protected $entityManager;
+	protected $eventManager;
 
-    public function __construct(EntityManager $entityManager) {
+	public function __construct(EntityManager $entityManager) {
+		if (!$this->entityClass) {
+			throw new NoEntityException('You must set an entity class');
+		}
+		$this->entityManager = $entityManager;
+	}
 
-        if (!$this->entityClass) {
-            throw new NoEntityException('You must set an entity class');
-        }
+	public function setEntityManager(EntityManager $entityManager) {
+		$this->entityManager = $entityManager;
+		return $this;
+	}
 
-        $this->entityManager = $entityManager;
-    }
+	public function getEntityManager() {
+		return $this->entityManager;
+	}
 
-    public function setEntityManager(EntityManager $entityManager) {
-        $this->entityManager = $entityManager;
-        return $this;
-    }
+	public function getEntityClass() {
+		return $this->entityClass;
+	}
 
-    public function getEntityManager() {
-        return $this->entityManager;
-    }
+	public function setEntityClass($entityClass) {
+		$this->entityClass = $entityClass;
+		return $this;
+	}
 
-    public function getEntityClass() {
-        return $this->entityClass;
-    }
+	public function setEventManager(EventManagerInterface $eventManager) {
+		$this->eventManager = $eventManager;
+	}
 
-    public function setEntityClass($entityClass) {
-        $this->entityClass = $entityClass;
-        return $this;
-    }
+	public function getEventManager() {
+		if (!$this->eventManager) {
+			$this->setEventManager(new EventManager(array(__CLASS__, get_called_class())));
+		}
+		return $this->eventManager;
+	}
 
-    public function setEventManager(EventManagerInterface $eventManager) {
-        $this->eventManager = $eventManager;
-    }
+	protected function getRepository() {
+		return $this->getEntityManager()->getRepository($this->entityClass);
+	}
 
-    public function getEventManager() {
-        if (!$this->eventManager) {
-            $this->setEventManager(new EventManager(array(__CLASS__, get_called_class())));
-        }
-        return $this->eventManager;
-    }
+	public function fetchOne($id) {
+		return $this->getRepository()->find($id);
+	}
 
-    protected function getRepository() {
-        return $this->getEntityManager()->getRepository($this->entityClass);
-    }
+	public function fetchAll() {
+		return $this->getRepository()->fetchAll();
+	}
 
-    public function fetchOne($id) {
-        return $this->getRepository()->find($id);
-    }
-
-    public function fetchAll() {
-        return $this->getRepository()->fetchAll();
-    }
-
-    public function fetchBy(array $criteria) {
-        return $this->getRepository()->findBy($criteria);
-    }
+	public function fetchBy(array $criteria) {
+		return $this->getRepository()->findBy($criteria);
+	}
 
 }
